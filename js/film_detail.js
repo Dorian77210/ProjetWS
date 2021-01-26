70c7a0fa0017d577299c59104f08066cc9addc14
YolannGauvin <56106849+YolannGauvin@users.noreply.github.com>
2021-01-26 11:11:25 +0100

    Modal popup

--- js/film_detail.js
+++ js/film_detail.js
@@ -62,94 +62,10 @@ async function loadFilm() {
     $img.setAttribute("src", imageURL);
     $img.classList.add("film-img");
     $imageContainer.append($img);
-
-
-    // Ajout des informations du film
-    //console.log("Abstract",filmData[0].country.value)
-
-    
-
-    
-
-    
-    //document.getElementById("country").innerHTML = countryName;
-    //document.getElementById("runtime").innerHTML = filmData[0].name.value;
-    //document.getElementById("language").innerHTML = countryName;
-    //document.getElementById("director").innerHTML = filmData[0].abstract.value;
-    //document.getElementById("abstract").innerHTML = filmData[0].abstract.value;
 }
-
 
 function convertSecToHour(timeSec){
     const hour = Math.floor(timeSec / 3600);
     const min = Math.floor((timeSec % 3600) / 60);
     return hour+':'+min;
 }
-
-
-/*function request()
-        {       
-
-            Informations du film :
-                - Titre/name (Original et autres)
-                - Catégorie
-                - Synopsis
-                - Acteurs
-                - Réalisateur
-                - Producteur
-                - Pays
-                - année de sortie
-                - Durée
-                - VO
-                - Auteur/Scénariste
-                - Montage
-                - Musique
-                - Photographie
-                - Société de production
-
-            var query = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf−schema#>"+
-                        "PREFIX dbo: <http://dbpedia.org/ontology/>"+
-                        "PREFIX dbp: <http://dbpedia.org/property/>"+
-
-                        "SELECT DISTINCT * WHERE {"+
-                        "?movie a dbo:Film ."+
-                        "?movie dbp:name ?name ."+
-                        "?movie dbo:abstract ?abstract ."+
-                        "?movie dbp:country ?country ."+
-                        //"?country dbp:label ?label ."+
-                        "?movie dbp:runtime ?runtime ."+
-                        "?movie dbp:language ?language ."+
-                        "?movie dbp:director ?director ."+
-                        "?movie dbp:producer ?producer ."+
-                        "?movie dbp:music ?music ."+
-                        "?movie dbp:distributor ?distributor ."+
-                        //"?movie dbp:image ?photo ."
-                        //"?movie dbp:language ?language ."+
-                        //"?movie dbp:language ?language ."+
-                        //"?movie dbp:language ?language ."+
-
-
-                        "filter regex(lcase(str(?name)) ,lcase('.*Titanic.*'))"+
-                        "filter langMatches(lang(?abstract), 'fr')"+
-                        "}";
-
-            var url = "http://dbpedia.org/sparql";
-            url =  url+"?query="+ encodeURIComponent(query) +"&format=json";
-
-            console.log(query);
-
-            return axios.get(url)
-                .then(res => {
-                    //console.log("résultat", res);
-                    var data = res.data.results.bindings;
-                    var countryParts = data[0].country.value.split('/');
-                    var countryName = countryParts[countryParts.length - 1];
-                    console.log("data", data[0], "\n\n", countryName)
-                    document.getElementById("name").innerHTML = data[0].name.value;
-                    document.getElementById("country").innerHTML = countryName;
-                    document.getElementById("abstract").innerHTML = data[0].abstract.value;
-                }).catch(err => console.log(err));
-            ;
-        }
-
-        request();*/
